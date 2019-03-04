c37e134781b3e6d93792c560e1c082988b3ceadd
Arvil Mena <arvilmena@gmail.com>
2019-03-04 14:59:55 +0000

    UserFixtures to populate 1 user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,15 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setUsername('test_1');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, '123456'));
+        $manager->persist($user);
 
         $manager->flush();
     }
