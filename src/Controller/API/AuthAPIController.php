31b16325ecdb55d0f8ee3a0bd4aecb3d229212b9
Arvil Mena <arvilmena@gmail.com>
2019-03-06 17:09:41 +0000

    can register => login => get token => use token

---
+++ src/Controller/API/AuthAPIController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class AuthAPIController extends AbstractController
+{
+    /**
+     * @Route("/api/auth/dashboard", name="api_auth")
+     */
+    public function index()
+    {
+        return new Response(sprintf('Logged in as %s', $this->getUser()->getUsername()));
+    }
+
+    /**
+     * @Route("/api/auth/register", name="api_auth_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $username = $request->request->get('_username');
+        $password = $request->request->get('_password');
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($encoder->encodePassword($user, $password));
+        $em->persist($user);
+        $em->flush();
+        return new Response(sprintf('User %s successfully created', $user->getUsername()));
+    }
+}
